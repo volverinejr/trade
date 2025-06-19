13f215f685b8712c6fc56e294f03cd2c5aa8cd4d
volverinejr <volverinejr@gmail.com>
2025-06-19 09:56:32 -0300

    configurando o testcontainer

--- src/test/java/br/com/claudemirojr/trade/CampeonatoServiceTest.java
+++ src/test/java/br/com/claudemirojr/trade/CampeonatoServiceTest.java
@@ -9,27 +9,18 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 
 import br.com.claudemirojr.trade.model.entity.Campeonato;
 import br.com.claudemirojr.trade.model.repository.CampeonatoRepository;
+import br.com.claudemirojr.trade.testcontainer.MySQLTestContainer;
 
 
 @SpringBootTest
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-public class CampeonatoServiceTest {
+public class CampeonatoServiceTest extends MySQLTestContainer {
 
     @Autowired
     private CampeonatoRepository campeonatoRepository;
-
-    @DynamicPropertySource
-    static void properties(DynamicPropertyRegistry registry) {
-        var mysql = MySQLTestContainer.getInstance();
-        registry.add("spring.datasource.url", mysql::getJdbcUrl);
-        registry.add("spring.datasource.username", mysql::getUsername);
-        registry.add("spring.datasource.password", mysql::getPassword);
-    }
 
     @BeforeEach
     void limparBds() {
@@ -43,7 +34,7 @@ public class CampeonatoServiceTest {
         campeonato.setDescricao("Principal competição de pontos corridos da Inglaterra");
         campeonato.setAtivo(true);
 
-        Campeonato salvo = campeonatoRepository.save(campeonato);
+        campeonatoRepository.save(campeonato);
         List<Campeonato> todos = campeonatoRepository.findAll();
         System.out.println("### Campeonatos no banco: " + todos);
         assertEquals(1, todos.size());
--- src/test/java/br/com/claudemirojr/trade/EquipeServiceTest.java
+++ src/test/java/br/com/claudemirojr/trade/EquipeServiceTest.java
@@ -9,41 +9,32 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 
 import br.com.claudemirojr.trade.model.entity.Equipe;
 import br.com.claudemirojr.trade.model.repository.EquipeRepository;
+import br.com.claudemirojr.trade.testcontainer.MySQLTestContainer;
 
 @SpringBootTest
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-class EquipeServiceTest {
+class EquipeServiceTest extends MySQLTestContainer {
 
-    @Autowired
-    private EquipeRepository equipeRepository;
+	@Autowired
+	private EquipeRepository equipeRepository;
 
-    @DynamicPropertySource
-    static void properties(DynamicPropertyRegistry registry) {
-        var mysql = MySQLTestContainer.getInstance();
-        registry.add("spring.datasource.url", mysql::getJdbcUrl);
-        registry.add("spring.datasource.username", mysql::getUsername);
-        registry.add("spring.datasource.password", mysql::getPassword);
-    }
+	@BeforeEach
+	void limparBds() {
+		equipeRepository.deleteAll();
+	}
 
-    @BeforeEach
-    void limparBds() {
-        equipeRepository.deleteAll();
-    }
+	@Test
+	void inserirEquipe() {
+		Equipe equipe = new Equipe();
+		equipe.setNome("Tottenham");
 
-    @Test
-    void inserirEquipe() {
-        Equipe equipe = new Equipe();
-        equipe.setNome("Tottenham");
-
-        Equipe salvo = equipeRepository.save(equipe);
-        List<Equipe> todos = equipeRepository.findAll();
-        System.out.println("### Equipes no banco: " + todos);
-        assertEquals(1, todos.size());
-        assertEquals("Tottenham", todos.get(0).getNome());
-    }
+		equipeRepository.save(equipe);
+		List<Equipe> todos = equipeRepository.findAll();
+		System.out.println("### Equipes no banco: " + todos);
+		assertEquals(1, todos.size());
+		assertEquals("Tottenham", todos.get(0).getNome());
+	}
 }
