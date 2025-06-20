7c2375cd4cb735418bbd3f0a0c69f65483518661
volverinejr <volverinejr@gmail.com>
2025-06-20 12:48:37 -0300

    obter token

--- src/test/java/br/com/claudemirojr/trade/EquipeServiceTest.java
+++ src/test/java/br/com/claudemirojr/trade/EquipeServiceTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
@@ -13,6 +14,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import br.com.claudemirojr.trade.model.entity.Equipe;
 import br.com.claudemirojr.trade.model.repository.EquipeRepository;
 import br.com.claudemirojr.trade.testcontainer.MySQLTestContainer;
+import br.com.claudemirojr.trade.util.AuthUtil;
 
 @SpringBootTest
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -20,12 +22,21 @@ class EquipeServiceTest extends MySQLTestContainer {
 
 	@Autowired
 	private EquipeRepository equipeRepository;
-
+	
+	 private String accessToken;
+	
+	
+	@BeforeAll
+    void obterToken() {
+		accessToken = AuthUtil.obterToken();
+    }
+	
+	
 	@BeforeEach
 	void limparBds() {
 		equipeRepository.deleteAll();
 	}
-
+	
 	@Test
 	void inserirEquipe() {
 		String nome = "Tottenham";
---
+++ src/test/java/br/com/claudemirojr/trade/util/AuthUtil.java
@@ -0,0 +1,44 @@
+package br.com.claudemirojr.trade.util;
+
+import br.com.claudemirojr.trade.dto.LoginRequestDto;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+public class AuthUtil {
+	
+	private static String cachedToken;
+	
+    public static String obterToken() {
+        if (cachedToken != null) {
+            return cachedToken;
+        }
+
+        RestAssured.useRelaxedHTTPSValidation();
+
+        String username = System.getenv("TEST_USERNAME");
+        String password = System.getenv("TEST_PASSWORD");
+
+        if (username == null || password == null) {
+            throw new IllegalStateException("Variáveis de ambiente TEST_USERNAME e TEST_PASSWORD devem estar definidas.");
+        }
+
+        String url = "https://grapp.tec.br/newapi/token";
+
+        LoginRequestDto loginRequest = new LoginRequestDto(username, password);
+
+        Response response = RestAssured.given()
+            .contentType("application/json")
+            .body(loginRequest)
+            .when()
+            .post(url)
+            .then()
+            .statusCode(200)
+            .extract()
+            .response();
+
+        cachedToken = response.jsonPath().getString("accessToken");
+
+        return cachedToken;
+    }	
+
+}
