ae090814b477b9c11b5f30fc1b57cf68c150473d
volverinejr <volverinejr@gmail.com>
2025-06-22 11:08:46 -0300

    mudanças test

---
+++ src/test/java/br/com/claudemirojr/trade/TradeApplicationTests.java
@@ -0,0 +1,17 @@
+package br.com.claudemirojr.trade;
+
+public class TradeApplicationTests {
+	
+/* TODO	
+	Clique com o botão direito no teste > Run As > Run Configurations.
+
+	Vá na aba Environment.
+
+	Adicione as variáveis manualmente.
+		REDIS_HOST = localhost
+		TEST_USERNAME
+		TEST_PASSWORD
+*/
+	
+
+}
